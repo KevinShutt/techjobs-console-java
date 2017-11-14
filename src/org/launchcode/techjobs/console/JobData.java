92806884ed0dd16c1f29560b79799073193a28cb
Kevin Shutt <kevinshutt@yahoo.com>
2017-11-13 18:47:00 -0600

    finished project

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -75,12 +76,40 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
+            // make search case-insensitive
+            value = value.toLowerCase();
+            aValue = aValue.toLowerCase();
 
             if (aValue.contains(value)) {
                 jobs.add(row);
             }
         }
 
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        //load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs) {
+
+                for(Map.Entry<String, String> column : job.entrySet()) {
+
+                    String aValue = column.getValue();
+                    // make search case-insensitive
+                    aValue = aValue.toLowerCase();
+                    value = value.toLowerCase();
+
+                    if (aValue.contains(value)) {
+                        jobs.add(job);
+                        break;
+                    }
+                }
+        }
         return jobs;
     }
 
